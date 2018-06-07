22f98f1ba8056e85e2a808e38de5ba557ab3cc9b
sirikops <sirikops@hotmail.com>
2018-06-07 21:11:14 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
 # dog.rb
+class Dog
+  
+  def initialize(bark, sit)
+    @bark=bark
+    @sit=sit
+    
+    def bark
+      @bark=bark
+    end
+    def bark=(bark)
+      @bark=bark
+    end
+    
+    def sit
+      @sit=sit
+    end
+    def sit=(sit)
+      @sit=sit
+    end
+   end
+     
+     dog1 = Dog.new("Woof!", "The Dog is sitting.")
+     
+     puts dog1
+     
+      
+    
+    
+  
+
+
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,22 @@
 # person.rb
+class Person
+  def initialize(talk, walk)
+    @talk=talk
+    @walk=walk
+    
+    def talk
+      @talk=talk
+    end
+    def talk=(talk)
+      @talk=talk
+    end
+    
+    def walk
+      @walk=walk
+    end
+    def walk=(walk)
+      @walk=walk
+    end
+  end
+  
+  person1 = Person.new("Hello World!", "The Person is walking.")
